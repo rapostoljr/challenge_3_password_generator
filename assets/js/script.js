526d8f3f6651a7ab471fcbc841e448fe37996505
RApostolJR <romuloapostol@gmail.com>
2024-02-21 22:17:31 -0800

    Temporarily added ACCEPTANCE CRITERIA to README.md, created generatePassword() function in script.js and added some code for it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,44 @@
 // Assignment code here
 var LOWERCASE_LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var UPPERCASE_LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+var NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]
+var SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+',]
 
-var passwordLength = prompt("How long do you want your password?")
-var containsLetters = confirm("Do you want your password to contain capital letters?")
-var containsNumbers = confirm("Do you want your password to contain numbers?")
-var containsSymbols = confirm("Do you want your password to contain special characters?")
 var generatedPassword = []
 
+function generatePassword() {
+  generatedPassword = []
+  console.log("Hey! You clicked the button!")
+  //  1. Prompt the user for how they want to make password
+  //    a. Length?
+  var passwordLength = prompt("How long do you want your password? (At least 8 characters and no more than 128 characters)")
+  if (passwordLength >= 8) {
+  //    b. Uppercase, lowercase, numbers, special letters?
+    var containsUpper = confirm("Do you want your password to contain capital letters?")
+      if (containsUpper) {
+        generatedPassword.push(LOWERCASE_LETTERS)
+        generatedPassword.push(UPPERCASE_LETTERS)
+      } else {
+        generatedPassword.push(LOWERCASE_LETTERS)
+      }
+    var containsNumbers = confirm("Do you want your password to contain numbers?")
+      if (containsNumbers) {
+        generatedPassword.push(NUMBERS)
+      }
+    var containsSymbols = confirm("Do you want your password to contain special characters?")
+      if (containsSymbols) {
+        generatedPassword.push(SYMBOLS)
+      }
+  } else {
+    alert("Please enter a valid response.")
+    generatePassword()
+  }
+  //  2. Validate the input.
+  
+  //  3. Generate password based on user's answers.
+  //  4. Display new generated password.
+  return `Enter variable ${generatedPassword} here, but randomized.`
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
